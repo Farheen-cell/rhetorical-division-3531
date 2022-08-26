a15f885c8ac3cfb644b9ef84fa90e857ceb69488
Farheen <farheen.ali108@gmail.com>
2022-08-26 14:43:41 +0530

    footer part

--- index.js
+++ index.js
@@ -1,143 +1,87 @@
-
-//-------------------------------------import nevbar--------------------------------------------
 import nevbar from "./components/nevbar.js";
 console.log(nevbar());
-document.getElementById("mainNevbar").innerHTML=nevbar();
-//---------------------------------------Best of Skin Store hidden div--------------------------
-let BestofSkinStore=document.getElementById("BoSS");
-let boss=document.getElementById('topbar');
-BestofSkinStore.addEventListener("mouseover",show_sidebar1);
-boss.addEventListener("mouseover",hide_sidebar1);
-document.getElementById('BoSS-h').style.visibility="hidden";
-    function show_sidebar1()
-    {
-    document.getElementById('BoSS-h').style.visibility="visible";
-    }
+document.getElementById("import").innerHTML=nevbar();
+import footer from "./components/footer.js";
+console.log(footer());
+document.getElementById("footerPart").innerHTML=footer();
+
+
+
+
+var slideIndex = 1;
+var timer;
+// var carouselBtn;
+
+function plusDivs(n) {
+    // console.log(n);
+    // clearInterval(timer);
+    showDivs(slideIndex += n);
+}
+
+function showDivs(n) {
+  var i;
+  var x = document.getElementsByClassName("slides");
+  var btn = document.getElementsByClassName("carousel__indicator");
+  if (n > x.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = x.length}
+  for (i = 0; i < x.length; i++) {
+    x[i].style.display = "none";
+    btn[i].classList.remove('carousel__indicator--active')
+
+  }
+  x[slideIndex-1].style.display = "block";  
+  btn[slideIndex-1].classList.add('carousel__indicator--active')
+}
+
+
+window.addEventListener('load', function(){
+    showDivs(slideIndex);
+    timer = setInterval(function(){
+        plusDivs(1)
+    }, 4000)
     
-    function hide_sidebar1()
-    {
-    document.getElementById('BoSS-h').style.visibility="hidden";
-    }
-   //--------------------------------------Summer Shop hidden div------------------------------------
-   let SummerShop=document.getElementById("SShop");
-   let sshop=document.getElementById('topbar')
-   SummerShop.addEventListener("mouseover",show_sidebar2);
- sshop.addEventListener("mouseover",hide_sidebar2);
-   document.getElementById('SummerShop').style.visibility="hidden";
-       function show_sidebar2()
-       {
-       document.getElementById('SummerShop').style.visibility="visible";
-       }
-       
-       function hide_sidebar2()
-       {
-       document.getElementById('SummerShop').style.visibility="hidden";
-       }
- //-----------------------------------------Sale--------------------------------------------------
- let Sale=document.getElementById("Sale");
- let sale=document.getElementById("topbar")
+})
 
- Sale.addEventListener("mouseover",show_sidebar3);
-sale.addEventListener("mouseover",hide_sidebar3);
- document.getElementById('sale').style.visibility="hidden";
-     function show_sidebar3()
-     {
-     document.getElementById('sale').style.visibility="visible";
-     Sale.style.backgroundColor="black"
-     }
-     
-     function hide_sidebar3()
-     {
-     document.getElementById('sale').style.visibility="hidden";
-     Sale.style.backgroundColor="white"
-     }    
-   //-----------------------------------BuildaRoutine--------------------------------------------------  
-   let BuildaRoutine=document.getElementById("BAR");
- let bar=document.getElementById("topbar")
 
- BuildaRoutine.addEventListener("mouseover",show_sidebar4);
-bar.addEventListener("mouseover",hide_sidebar4);
- document.getElementById('BuildaRoutine').style.visibility="hidden";
-     function show_sidebar4()
-     {
-     document.getElementById('BuildaRoutine').style.visibility="visible";
-     BuildaRoutine.style.backgroundColor="black"
-     }
-     
-     function hide_sidebar4()
-     {
-     document.getElementById('BuildaRoutine').style.visibility="hidden";
-     BuildaRoutine.style.backgroundColor="white"
-     } 
-   //--------------------------------------SkinCare--------------------------------------
-   let SkinCare=document.getElementById("SC");
-   let sc=document.getElementById("topbar")
-  
-   SkinCare.addEventListener("mouseover",show_sidebar5);
-  sc.addEventListener("mouseover",hide_sidebar5);
-   document.getElementById('SkinCare').style.visibility="hidden";
-       function show_sidebar5()
-       {
-       document.getElementById('SkinCare').style.visibility="visible";
-       SkinCare.style.backgroundColor="black"
-       }
-       
-       function hide_sidebar5()
-       {
-       document.getElementById('SkinCare').style.visibility="hidden";
-       SkinCare.style.backgroundColor="white"
-       }
-       //------------------------------------------Hair----------------------------------------------
-       let hair=document.getElementById("HR");
-       let hr=document.getElementById("topbar")
-      
-       hair.addEventListener("mouseover",show_sidebar6);
-      hr.addEventListener("mouseover",hide_sidebar6);
-       document.getElementById('Hair').style.visibility="hidden";
-           function show_sidebar6()
-           {
-           document.getElementById('Hair').style.visibility="visible";
-          hair.style.backgroundColor="black"
-           }
-           
-           function hide_sidebar6()
-           {
-           document.getElementById('Hair').style.visibility="hidden";
-           hair.style.backgroundColor="white"
-           }
-//------------------------------------Makeup-----------------------------------------------
-let makeup=document.getElementById("MK");
-       let mk=document.getElementById("topbar")
-      
-       makeup.addEventListener("mouseover",show_sidebar7);
-      mk.addEventListener("mouseover",hide_sidebar7);
-       document.getElementById('Makeup').style.visibility="hidden";
-           function show_sidebar7()
-           {
-           document.getElementById('Makeup').style.visibility="visible";
-          makeup.style.backgroundColor="black"
-           }
-           
-           function hide_sidebar7()
-           {
-           document.getElementById('Makeup').style.visibility="hidden";
-           makeup.style.backgroundColor="white"
-           }  
-  //------------------------------------------------Tools--------------------------------------     
-  let tools=document.getElementById("TS");
-       let tl=document.getElementById("topbar")
-      
-       tools.addEventListener("mouseover",show_sidebar8);
-      tl.addEventListener("mouseover",hide_sidebar8);
-       document.getElementById('Tools').style.visibility="hidden";
-           function show_sidebar8()
-           {
-           document.getElementById('Tools').style.visibility="visible";
-          tools.style.backgroundColor="black"
-           }
-           
-           function hide_sidebar8()
-           {
-           document.getElementById('Tools').style.visibility="hidden";
-           tools.style.backgroundColor="white"
-           }              
+
+
+var slideIndexes = 1;
+
+function plusDivs1(a) {
+// console.log(n);
+// clearInterval(timer);
+showDivs1(slideIndexes += a);
+}
+
+function showDivs1(a) {
+var j;
+var xe = document.getElementsByClassName("slick-slides");
+var btn1 = document.getElementsByClassName("slick__indicator");
+if (a > xe.length) {slideIndexes = 1}
+if (a < 1) {slideIndexes = xe.length}
+for (j = 0; j < xe.length; j++) {
+xe[j].style.display = "none";
+btn1[j].classList.remove('slick__indicator--active')
+
+}
+xe[slideIndexes-1].style.display = "block";  
+btn1[slideIndexes-1].classList.add('slick__indicator--active')
+}
+
+
+window.addEventListener('load', function(){
+showDivs1(slideIndexes);
+// timer = setInterval(function(){
+// plusDivs(1)
+// }, 4000)
+
+})
+
+var pricenavbarcart = document.getElementById('nav_cart_price');
+
+var priceStr = "$0.00"
+if(JSON.parse(localStorage.getItem('finalPrice')) !== null){
+  var priceStr = "$"+JSON.parse(localStorage.getItem('finalPrice'));
+}
+
+pricenavbarcart.innerText = priceStr;
